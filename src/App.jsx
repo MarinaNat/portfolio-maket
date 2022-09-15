0e9cbbe545b419cc380e86965c142933097a31bf
Marina Natalina <Kiska21mk@ya.ru>
2022-09-15 18:03:49 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.scss';
 import Header from './components/elements/Header/Header';
+import Main from './components/elements/Main/Main';
 
 function App() {
   return (
     <div className='wrapper'>
       <Header />
+      <div className='main'>
+        <Main />
+      </div>
     </div>
-
   );
 }
 
